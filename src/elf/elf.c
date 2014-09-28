94ee1c09c0a64c0f2d5ce144cdc8af76ddc1ec6d
Fabio Battaglia <hkzlabnet@gmail.com>
2014-09-28 07:54:32 +0200

    Begin adding elf controls

--- src/elf/elf.c
+++ src/elf/elf.c
@@ -1,1 +1,16 @@
 #include "elf/elf.h"
+
+void elf_setControlSwitches(uint8_t sw_bitmask) {
+	uint8_t ctrl_bitmask = 0;
+
+ 	// Correctly convert the bitmask	
+	ctrl_bitmask |= ((sw_bitmask & 0x8) ? 0x00 : 0x01); // IN connected to /STROBE, must be reversed
+	ctrl_bitmask |= ((sw_bitmask & 0x4) ? 0x00 : 0x02); // WAIT connected to /AUTOFEED, must be reversed
+	ctrl_bitmask |= ((sw_bitmask & 0x2) ? 0x04 : 0x00); // CLR connected to INIT, must NOT be reversed
+	ctrl_bitmask |= ((sw_bitmask & 0x1) ? 0x08 : 0x00); // R/W connected to /SELECT, must be reversed (UP generates a GND signal, which means Write Enable)
+
+	parport_write(PP_CONTROL, ctrl_bitmask);
+}
+
+void elf_setDataSwitches(uint8_t sw_bitmask) {
+}
--- src/elf/elf.h
+++ src/elf/elf.h
@@ -4,4 +4,21 @@
 #include "common/defines.h"
 #include "common/cmn_datatypes.h"
 
+#include "parport/parport.h"
+
+/* 0 -> Switch flipped down, 1 -> Switch flipped up
+ * First 4 bits are ignored
+ * - IN switch
+ * - WAIT
+ * - CLR
+ * - R/W
+ */
+void elf_setControlSwitches(uint8_t sw_bitmask);
+
+/* 0 -> Switch flipped down, 1 -> Switch flipped up
+ * All bits are used.
+ * - MSb is D7, LSb is D0
+ */
+void elf_setDataSwitches(uint8_t sw_bitmask);
+
 #endif /* _ELF_HEADER_ */
